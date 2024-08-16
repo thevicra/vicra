367d19a1d7126791dd65a3219c43cc90f28070df
thevicra <info.vicra@gmail.com>
2024-08-16 10:02:42 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -11,9 +11,9 @@
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
-  } else {
+} else {
     die( 'Unable to load the "PHP Email Form" Library!');
-  }
+}
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
